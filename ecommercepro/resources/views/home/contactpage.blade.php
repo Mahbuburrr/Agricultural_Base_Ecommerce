e0117af649585530c88c2d7e9d981189fcbd5f81
Mahbubur Rahman <mahbuburcse191@gmail.com>
2023-07-17 22:26:03 +0600

    first commit

---
+++ ecommercepro/resources/views/home/contactpage.blade.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <!-- Basic -->
+      <!-- Basic -->
+      <meta charset="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <!-- Mobile Metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      <!-- Site Metas -->
+      <meta name="keywords" content="" />
+      <meta name="description" content="" />
+      <meta name="author" content="" />
+      <link rel="shortcut icon" href="images/favicon.png" type="">
+      <title>agro service</title>
+      
+
+      <!-- bootstrap core css -->
+      <link rel="stylesheet" type="text/css" href="{{asset('home/css/bootstrap.css')}}" />
+      <!-- font awesome style -->
+      <link href="{{asset('home/css/font-awesome.min.css')}}" rel="stylesheet" />
+      <!-- Custom styles for this template -->
+      <link href="{{asset('home/css/style.css')}}" rel="stylesheet" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+      <!-- responsive style -->
+      <link href="{{asset('home/css/responsive.css')}}" rel="stylesheet" />
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+   </head>
+   <body>
+      <div class="hero_area">
+         <!-- header section strats -->
+         @include('home.header')
+         <!-- end header section -->
+         <!-- slider section -->
+        
+      @include('home.contact')
+    
+
+      <!-- subscribe section -->
+      <!-- @include('home.subscribe') -->
+      <!-- end subscribe section -->
+      <!-- client section -->
+      <!-- @include('home.client') -->
+      <!-- end client section -->
+      <!-- footer start -->
+
+
+      @include('home.footer')
+      <!-- footer end -->
+      <div class="cpy_">
+         <p class="mx-auto">Agro <a href="https://html.design/">Web-Service</a><br>
+         
+            Distributed By <a href="https://themewagon.com/" target="_blank">Mahbubur Rahman</a>
+         
+         </p>
+      </div>
+
+      <script>
+         function reply(caller){
+
+            document.getElementById('commentId').value=$(caller).attr('data-commentid');
+            $('.replyDiv').insertAfter($(caller));
+            $('.replyDiv').show();
+         }
+
+         function reply_close(caller){
+
+
+$('.replyDiv').hide();
+}
+      </script>
+
+<script>
+        document.addEventListener("DOMContentLoaded", function(event) { 
+            var scrollpos = localStorage.getItem('scrollpos');
+            if (scrollpos) window.scrollTo(0, scrollpos);
+        });
+
+        window.onbeforeunload = function(e) {
+            localStorage.setItem('scrollpos', window.scrollY);
+        };
+    </script>
+
+
+
+
+      <!-- jQery -->
+      <script src="home/js/jquery-3.4.1.min.js"></script>
+      <!-- popper js -->
+      <script src="home/js/popper.min.js"></script>
+      <!-- bootstrap js -->
+      <script src="home/js/bootstrap.js"></script>
+      <!-- custom js -->
+      <script src="home/js/custom.js"></script>
+
+      <script>
+var botmanWidget = {
+    aboutText:'wrbappfix', 
+};
+</script>
+
+
+   </body>
+   <script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js'></script>
+</html>
